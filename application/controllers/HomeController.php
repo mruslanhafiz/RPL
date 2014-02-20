543c48320e46c03bb99b7500fe59601d7876bb63
m ruslan hafiz <mruslanhafiz@live.co.uk>
2014-02-20 23:48:12 +0700

    nih bes

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -1,8 +1,17 @@
 <?php
 
 class HomeController extends CI_Controller{
-    public function method(){
-        echo "hai hai";
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function index(){
+		$this->load->view('main');
+
+	}
+    public function method($a = 'jkl', $z = 'nnnn'){
+        echo $a . $z;
     }
 }
 ?>
