9b1b84cc700625c06c45499b524a8fc3fe9f9527
Bestama Abhi Priambada <bestama.abhi@windowslive.com>
2014-02-21 00:15:47 +0700

    tambah controller view semuanya

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -2,15 +2,43 @@
 
 class HomeController extends CI_Controller{
 
-	public function __construct() {
+	public function __construct() 
+	{
 		parent::__construct();
 	}
 
-	public function index(){
-		$this->load->view('main');
-
+	public function index()
+	{
+		$this->load->view('index');
 	}
-    public function method($a = 'jkl', $z = 'nnnn'){
+
+	public function main()
+	{
+		$this->load->view('main');
+	}
+
+	public function bookings()
+	{
+		$this->load->view('bookings');
+	}
+
+	public function login()
+	{
+		$this->load->view('login');
+	}
+
+	public function tambahPC()
+	{
+		$this->load->view('tambah_PC');
+	}
+
+	public function updatePC()
+	{
+		$this->load->view('update_PC');
+	}
+
+    public function method($a = 'jkl', $z = 'nnnn')
+    {
         echo $a . $z;
     }
 
