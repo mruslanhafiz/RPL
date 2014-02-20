a78c0a5524bb519676a01e9afd8df5cf4cc0d58b
m ruslan hafiz <mruslanhafiz@live.co.uk>
2014-02-21 00:04:02 +0700

    nambah viewnya

---
+++ application/views/bookings.php
@@ -0,0 +1,46 @@
+<!doctype html>
+
+<html>
+  
+  <head>
+    <title>New Page</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+  </head>
+  
+  <body>
+    <div class="container">
+      <h1>Book Form</h1>
+      <div class="form-group">
+        <label class="control-label">Nama</label>
+        <div class="controls">
+          <input type="text" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label">NRP</label>
+        <div class="controls">
+          <input type="text" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label">No.HP</label>
+        <div class="controls">
+          <input type="text" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label">Email</label>
+        <div class="controls">
+          <input type="text" class="form-control">
+        </div><br>
+        <div class="controls">
+          <a class="btn btn-primary">Submit</a>
+        </div>
+      </div>
+    </div>
+  </body>
+
+</html>
---
+++ application/views/tambah_PC.php
@@ -0,0 +1,43 @@
+<!doctype html>
+
+<html>
+  
+  <head>
+    <title>New Page</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+  </head>
+  
+  <body>
+    <div class="container">
+      <h1>Tambah PC</h1>
+      <div class="form-group">
+        <label class="control-label">Nama PC</label>
+        <div class="controls">
+          <input type="text" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label">Spesifikasi</label>
+        <div class="controls">
+          <input type="text" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label">Pemilik&nbsp;</label>
+        <div class="controls">
+          <input type="text" class="form-control">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="controls"></div><br>
+        <div class="controls">
+          <a class="btn btn-primary">Submit</a>
+        </div>
+      </div>
+    </div>
+  </body>
+
+</html>
