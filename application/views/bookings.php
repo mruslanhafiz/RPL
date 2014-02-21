ea187d7a92cd8dac26d96509a0fd4915a44b893c
Bestama Abhi Priambada <bestama.abhi@windowslive.com>
2014-02-21 18:42:34 +0700

    booking

--- application/views/bookings.php
+++ application/views/bookings.php
@@ -13,33 +13,44 @@
   <body>
     <div class="container">
       <h1>Book Form</h1>
+      <form action= <?php /*
+      $con=mysqli_connect("localhost","root","root","rpl");
+        if (mysqli_connect_errno())
+        {
+          echo "Failed to connect : ". mysqli_connect_error();
+        }
+        mysqli_query($con,"insert into data_pc 
+          (nama_user, hp_user, email_user, tanggal_pinjam)
+          values ('" . $nama . "','" . nama . "','" . nama . "','" . nama . "','" . nama . "');");
+      echo "main"*/?>>
       <div class="form-group">
         <label class="control-label">Nama</label>
         <div class="controls">
-          <input type="text" class="form-control">
+          <input type="text" class="form-control" name="nama">
         </div>
       </div>
       <div class="form-group">
         <label class="control-label">NRP</label>
         <div class="controls">
-          <input type="text" class="form-control">
+          <input type="text" class="form-control" name="nrp">
         </div>
       </div>
       <div class="form-group">
         <label class="control-label">No.HP</label>
         <div class="controls">
-          <input type="text" class="form-control">
+          <input type="text" class="form-control" name="hp">
         </div>
       </div>
       <div class="form-group">
         <label class="control-label">Email</label>
         <div class="controls">
-          <input type="text" class="form-control">
+          <input type="text" class="form-control" name="email">
         </div><br>
         <div class="controls">
-          <a class="btn btn-primary">Submit</a>
+          <input type="submit" class="btn btn-primary" value="Submit">
         </div>
       </div>
+    </form>
     </div>
   </body>
 
